bab2910b6243eacf969ff08811eadb20b6bab592
shivangi <shivangi.das7@gmail.com>
2018-08-02 23:29:06 +0530

    ui changes

--- app/models/index.js
+++ app/models/index.js
@@ -1,43 +1,53 @@
 "use strict";
-const fs = require('fs')
-const path = require('path')
-const Sequelize = require('sequelize')
-const config = require('../config/config')
-const db = {}
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const config = require("../config/config");
+const db = {};
 const Op = Sequelize.Op; //remove the warning message
 
-
 // Database
-/* const sequelize = new Sequelize(config.db.database, config.db.user, config.db.password, {
-    host: config.db.host,
-    dialect: 'postgres',
-    logging: false, //the query that is logged in console. remove when debugging
-    operatorsAliases: Op
-}); */
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
+let sequelize;
+if (process.env.NODE_ENV == "production") {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
     operatorsAliases: Op,
     logging: false, //the query that is logged in console. remove when debugging
     dialectOptions: {
-        ssl: true
+      ssl: true
     }
-});
+  });
+} else {
+  sequelize = new Sequelize(
+    config.db.database,
+    config.db.user,
+    config.db.password,
+    {
+      host: config.db.host,
+      dialect: "postgres",
+      logging: false, //the query that is logged in console. remove when debugging
+      operatorsAliases: Op
+    }
+  );
+}
 
 fs.readdirSync(__dirname)
-    .filter((file) =>
-        //file !== 'index.js'
-        (file.indexOf(".") !== 0) && (file !== "index.js" && file !== "relations.js" )
-    )
-    .forEach((file) => {
-        const model = sequelize.import(path.join(__dirname, file))
-        db[model.name] = model
-    });
+  .filter(
+    file =>
+      //file !== 'index.js'
+      file.indexOf(".") !== 0 &&
+      (file !== "index.js" && file !== "relations.js")
+  )
+  .forEach(file => {
+    const model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
+  });
 
 // relationships/associations described here
-require('./relations')(db)
+require("./relations")(db);
 
-db.sequelize = sequelize
-db.Sequelize = Sequelize
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-module.exports = db
+module.exports = db;
