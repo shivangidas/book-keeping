dce5cb27e68b8f8a1fb12f49971d1c9f2d91342f
shivangi <shivangi.das7@gmail.com>
2018-08-12 00:39:27 +0530

    routing issues

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -9,9 +9,7 @@ module.exports = function(app, secureRoutes) {
       next();
     }
   };
-  app.get("/poseNet", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-  });
+
   app
     .route("/signup")
     .get(sessionChecker, (req, res) => {
@@ -99,6 +97,9 @@ module.exports = function(app, secureRoutes) {
     } else {
       res.redirect("/login");
     }
+  });
+  app.get("/poseNet", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
   });
   //apis
   const bookController = require("../controllers/bookController");
